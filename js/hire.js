83a437be99b24e2a6e3fffc9a9d846da99eb8f24
Christine <you@example.com>
2019-08-13 11:09:31 -0400

    first commit

---
+++ js/hire.js
@@ -0,0 +1,99 @@
+function hideNext(){
+    var y = document.getElementById("noOne");
+    var z = document.getElementById("questionTwo");
+    if (z.style.display === "block") {
+      z.style.display = "none";
+    } else {
+      document.getElementById("sorryTwo").innerHTML = "Looks like we're not a good match! Womp womp :(";
+    }
+  }
+  
+  function functionOne() {
+    var x = document.getElementById("questionOne");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function functionTwo() {
+    var x = document.getElementById("questionTwo");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function functionThree() {
+    var x = document.getElementById("questionThree");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function functionFour() {
+    var x = document.getElementById("questionFour");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function functionFive() {
+    var x = document.getElementById("questionFive");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function functionSix() {
+    var x = document.getElementById("questionSix");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function functionSeven() {
+    var x = document.getElementById("questionSeven");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+    function functionEight() {
+    var x = document.getElementById("questionEight");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function whyNot() {
+    var x = document.getElementById("whyNot");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
+  function sorry() {
+    var x = document.getElementById("sorryAll");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
